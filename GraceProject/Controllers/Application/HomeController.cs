d5a61c5d19ccc4931c135a29dca8cec190eba637
Aparana Pant <aparanapant@gmail.com>
2025-06-26 18:18:25 -0500

    add appseetings

---
+++ GraceProject/Controllers/Application/HomeController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+[Route("AppSettings")]
+public class AppSettingsController : Controller
+{
+    [Route("")]
+    public IActionResult Index()
+    {
+        return View("~/Views/Application/index.cshtml");
+    }
+}
